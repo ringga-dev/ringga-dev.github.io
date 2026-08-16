d8d808161bdd9b73cfd1acda8ad753cda87f2724
ringga-dev <ringga.dev@gmail.com>
2026-08-16 20:58:25 +0800

    chore: remove debug-routing.vue (unused debug page)

--- src/pages/debug-routing.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding: 100px; text-align: center; background: white; color: black;">
-    <h1>ROUTING TEST PAGE</h1>
-    <p>If you can see this, routing is working.</p>
-    <NuxtLink href="/">Go Home</NuxtLink>
-  </div>
-</template>
